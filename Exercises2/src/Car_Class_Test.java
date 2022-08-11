56fa72e7045bddf77ccad4a2425ab379f0c4f20b
Michal <michal.matura@upmraflatac.com>
2022-08-11 22:26:40 +0200

    Exercises2 last update, class created

---
+++ Exercises2/src/Car_Class_Test.java
@@ -0,0 +1,23 @@
+public class Car_Class_Test {
+
+        public static void makeNoise() {
+            String result = "result";
+            {
+                result = "result";
+            }
+            System.out.println(result);
+    }
+}
+
+
+
+//public class Car {
+//
+//    public void makeNoise () {
+//
+//        {
+//            String result = "result";
+//        }
+//        System.out.println(result);
+//    }
+//}
--- Exercises2/src/Main.java
+++ Exercises2/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+
+        Car_Class_Test objectCar = new Car_Class_Test();
+        Car_Class_Test.makeNoise();
+
+
+
+}
 }
