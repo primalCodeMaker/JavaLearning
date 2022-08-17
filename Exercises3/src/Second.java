cd4511bf32425b52a1cf0a21b5e7828172c1219b
primalCodeMaker <binaryCodeMaker@gmail.com>
2022-08-17 17:11:03 +0200

    Exercisec3 added

---
+++ Exercises3/src/Second.java
@@ -0,0 +1,23 @@
+public class Second {
+    // Napisz program w którym użyjesz operatorów ==, !, !=, ++, -=, *=. Na początku masz zadeklarowaną
+    //zmienną int x = 0. Podczas pisania programu nie możesz zadeklarować żadnej innej zmiennej, tzn.
+    //przez cały czas pisania programu możesz operować tylko na tej jednej zmiennej.
+
+    public static void main(String[] args) {
+        Second second = new Second();
+        second.equals();
+
+
+    }
+
+        private void equals () {
+            int x = 0;
+            if (x >= 10) {
+                System.out.println("1st step");
+            } else {
+                System.out.println("2nd step");
+            }
+
+        }
+
+}
