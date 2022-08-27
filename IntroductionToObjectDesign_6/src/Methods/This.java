9706368613d16d2062bfeae1c3f7be1c7022c029
primalCodeMaker <binaryCodeMaker@gmail.com>
2022-08-27 09:53:02 +0200

    'this' in method finished

--- IntroductionToObjectDesign_6/src/Methods/This.java
+++ IntroductionToObjectDesign_6/src/Methods/This.java
@@ -8,12 +8,23 @@ public class This {
         This country2 = new This();
         This country3 = new This();
 
-        country2.country = "Poland"; //todo jak tu sie odniesc do pola w klasie?
+        country2.country = "Poland"; // pole klasy musialo by byc statyczne aby mozna bylo przypisac tutaj jego wartosc
+                                     // z kolei jesli pole klasy bedzie statyczne to nie bedzie mozna uzyc tu thisa...
 
         country1.wchichCountry(country1.country);
         System.out.println(country2.country);
 
-        country2.thisCantBeUseInStaticMethod(country2.country);
+        country2.thisCannotBeUseInStaticMethod(country2.country);
+
+       country3.country = "i dont really know what am i doin dude ...";
+        System.out.println(country3.country);
+        country3.thisCannotBeUseInStaticMethod(country3.country);
+        country3.thisExampleToOverwriteName(country3.country);
+        country3.thisExamplewithThisUsed(); // tutaj nie uzylem argumentow.
+        // gdybym 2  liniji wyzej zrobil metode, ktora nie uzywa argumentow, to nadpisal bym domyslna wartosc country3.country
+        // i po uzyciu thisa w metodzie pozniej juz by uzywalo docelowej wartosci country3.country = anythink else z metody bez argumentow
+
+
     }
 
     public void wchichCountry (String country) {
@@ -21,12 +32,19 @@ public class This {
         System.out.println(country);
     }
 
-    public void thisCantBeUseInStaticMethod (String country) {
+    public void thisCannotBeUseInStaticMethod (String country) {
         country = "AHTUNG POGANS!";
-        System.out.println(this.country);
+        System.out.println(country);
         // this wskazuje na to, ze nie chodzi nam o parametr metody, tylko o pole w klasie
         // this = odwoluje sie do pola obiektu, na ktorym w tym momencie operuje
 
     }
-
+    public void thisExampleToOverwriteName (String country) {
+        country = "anythink else";
+        System.out.println(country);
+    }
+    public void thisExamplewithThisUsed () {
+        country = this.country;
+        System.out.println(this.country);
+    }
 }
