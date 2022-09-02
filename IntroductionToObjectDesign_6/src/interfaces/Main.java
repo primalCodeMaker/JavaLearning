3bb5ae64c5890b1474e9dd2bc8e5dae323b6cb8d
primalCodeMaker <binaryCodeMaker@gmail.com>
2022-09-02 22:37:28 +0200

    interface rules updated

--- IntroductionToObjectDesign_6/src/interfaces/Main.java
+++ IntroductionToObjectDesign_6/src/interfaces/Main.java
@@ -1,12 +1,15 @@
 package interfaces;
+// zestaw zadeklarowanych metod, zazwyczaj bez działania, ktore są implementowane w klasie
+    // i dopierow tej klasie są opisywane co maja dokladnie robic
+    // metodom w interfejsie można nadawać wartości default, ktore zostana wywolane w przypadku gdy taka nie zostanie nadpisana w klasie konkretnej
 
-// kazda metoda w interfejsie jest public abstract, mimo ze nie jest to definiowane
+// kazda metoda w interfejsie jest PUBLIC ABSTRACT, mimo ze nie jest to definiowane
     // w klasie ktora implementuje interfejsy nie mozna zawezic zakresu widocznosci takiej metody
 
 // klasa, ktora implementuje interfejs musi przyjac wszystkie jego metody
 
 // klasa moze implementować więcej niz 1 interfejs
-// ale nie moze dziedziczyc tylko z 1 klasy
+    // ale moze dziedziczyc tylko z 1 klasy
 
 // Marker Interface - to pusty interfejs, i klasy, które go implementują są "specjalne" w jakiś sposób
 
@@ -14,7 +17,10 @@ package interfaces;
 // metoda default nie moze byc abstrakcyjna
 
 //metoda w interfejsie MOZE być private, ale mozna wtedy ja uzywac tylko w tym interfejsie
-    //np: wywolac w innej metodzie, ktora bedzie zwracać default jeśli nie zostanie nadpisana
+    //np: wywolac w innej metodzie, jako wattość default jeśli nie zostanie nadpisana
+
+// interfejsy moga sie wzajemnie dziedziczyc, nawet z kilku klas czego nie mozna zrobić na klasie
+// interfejs nie moze implementowac inne interfejsy
 
 public class Main {
 
---
+++ IntroductionToObjectDesign_6/src/interfaces/SecondExample/Main.java
@@ -0,0 +1,10 @@
+package interfaces.SecondExample;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
