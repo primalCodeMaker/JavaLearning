183bd67fcd9c62e00fb16855fb406774ccaf4f0b
primalCodeMaker <binaryCodeMaker@gmail.com>
2022-09-27 23:06:44 +0200

    debuging fun

--- IntroductionToObjectDesign_6/src/enums/Info.java
+++ IntroductionToObjectDesign_6/src/enums/Info.java
@@ -9,4 +9,5 @@ public class Info {
     // w Enumach nie mozna @Overridowac ToStringa
     // w Enumie mozna pisac metody abstrakcyjne. Kazdy z Enumow moze nadpisywac wlasna implementacje metod abstrakcyjnych
         // w ten sposob mozemy za pomoca takiej metody zwracac innego Enuma w Enumie (return PASSAT)
+
 }
