6fd542db32019357eba733d093f30a4236234e07
primalCodeMaker <binaryCodeMaker@gmail.com>
2022-10-27 19:20:52 +0200

    memory model in java

---
+++ Exercises11/src/second/Info.java
@@ -0,0 +1,10 @@
+package second;
+
+public class Info {
+
+    // Napisz program, który losowo zamieni kolejność elementów w podanej liście.
+    // Do osiągnięcia efekt losowości spróbuj wykorzystać klasę Random.
+    // Wiem, że nie poruszaliśmy jej w materiałach, dlatego postaraj się wykorzystać google żeby znaleźć przykłady jej użycia.
+    // Od razu wyjaśnię, że robię to w ten sposób, żeby dać Ci możliwość ćwiczenia szukania informacji na własną rękę,
+    // bo w pracy to jest codzienność :)
+}
