d17a674292380da0394265e244dff53d398a67f4
primalCodeMaker <binaryCodeMaker@gmail.com>
2022-10-28 06:45:26 +0200

    data and time added

---
+++ Date_and_Time/src/Info.java
@@ -0,0 +1,10 @@
+public class Info {
+}
+// od javy 8 stara kalsa java.util.date została zastąpiona java.time
+// istnieja podstawowe klasy i każda z nich ma inne dostepne metody:
+    // localDate
+    // localTime
+    // localDateTime
+
+// konstruktor w tych klasach jest prywatny, to znaczy ze nie mozna zrobić nowego obiektu każdej z tych klas
+// aby kozystac z kasy trzeba uzyc statycznej metody .of()
---
+++ Date_and_Time/src/localDate_and_localTime/Time.java
@@ -0,0 +1,25 @@
+package localDate_and_localTime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class Time {
+
+    public static void main(String[] args) {
+        System.out.println("LocalDate.now " + LocalDate.now());
+        System.out.println("LocalTime.now " + LocalTime.now());
+        System.out.println("LocalDateTime.now " + LocalDateTime.now());
+        System.out.println();
+
+        System.out.println("LocalDate.of " + LocalDate.of(2020, 11, 2));
+        System.out.println("LocalDate.of " + LocalDate.of(2020, Month.OCTOBER, 2));
+        System.out.println("LocalTime.of " + LocalTime.of(17, 50));
+        System.out.println("LocalTime.of " + LocalTime.of(17,50,20));
+        System.out.println("LocalTime.of " + LocalTime.of(17,50,20,902));
+
+        System.out.println("LocalDateTime.of " + LocalDateTime.now());
+
+    }
+}
