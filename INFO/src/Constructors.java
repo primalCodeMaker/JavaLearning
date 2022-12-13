eda8602f2d81e9c57bc2f35e5b81737cf397fbe3
primalCodeMaker <binaryCodeMaker@gmail.com>
2022-12-13 18:47:44 +0100

    Exceptions done

--- INFO/src/Constructors.java
+++ INFO/src/Constructors.java
@@ -4,5 +4,11 @@ public class Constructors {
 // this() w odwolaniu do konstruktorow. Musi byc jako pierwsze. Jesli piszemy this to super juz nie dziala
 //	this() odwoluje sie do overlodowanego konstruktora w klasie. Trzeba podac odpowiednie argumenty
 
-//super() w odwolaniu do konstruktorow. Musi byc jako pierwsze wyrazenie w konstruktorze. W dziedziczeniu
-//	jest to niejawnie zapisane.
+//super() w odwolaniu do konstruktorow. Musi byc jako pierwsze wyrazenie w konstruktorze.
+// W dziedziczeniu super() jest zawsze niejawnie zapisane,
+// mozna to nadpisac poprzez podanie argumentow z konstruktora klasy bazowej
+
+
+// Konstruktorow nie da sie @Overrideowac bo konstruktor jest danej klasy
+
+// konstruktory mozna Overloadowac
--- INFO/src/Exceptions.java
+++ INFO/src/Exceptions.java
@@ -8,7 +8,8 @@ public class Exceptions {
     // wymagaja obslugi Try {} Cath {}
 
 // Wyjatki mozna wyzucac wyzej, aby zostaly obsluzone na pozniejszym etapie kompilacji a nie bezposrednio
-// np: w metodzie ktora generuje wyjatek
+// np: w metodzie ktora generuje wyjatek,
+// uzywa sie do tego slowa kluczowego throws za sygnatura metody + typ wyjatku jaki ma byc rzucany
 
 // StakTrace - Wyswietla jaki wyjatek zostal wywolany oraz kazdy punkt wywolania
 
@@ -37,9 +38,31 @@ public class Exceptions {
     // pod warunkiem ze chcemy zucic bardziej zagniezdzony wyjatek w hierarchi dziedziczenia
     // w 2 strone nie da sie overrideowac
     // nie mozna tez wybrac innego bledu typu Catch, jesli nie jest on dziedziczony
-    // Bledy typu Runtime mozemy dodawac
+    // Bledy typu Runtime mozemy dodawac dowolnie
 
 
+// Mozna zucac wyjatki w Konstruktorach w zwyklych klasach np za pomoca if()
+    // i beda one wywolywane na poziomie tworzenia obiektu jesli bedzie to Runtime
+// Jesli taki wyjatek bedzie typu catch to trzeba bedzie obsluzyc w konstruktorze
+// albo rzucic wyzej za pomoca throws.
+// Wyzej mozna przekazywac Exceptiony, ktore sa bardziej ogolne w hierarhii dziedziczenia
+
+// w klasach rozszerzajacych Exceprion tez mozna robic konstruktory i w argumencie wpisac np: String message
+    // ten message bedzie zwracal naszego Stringa podczas tworzenia i wywolania wyjatku
+
+
+// W bloku inicjalizacyjnym zwyklym i statycznym mozna pisac wyjatki
+// Sa one przydatne podczas ladowania plikow z zewnatrz. Zazwyczaj sa to wyjatki typu Catch
+// W blokach instancyjnych trzeba dopisac throws do wszystkich konstruktorow kalsy
+// w blokach statycznych od razu trzeba obsluzyc taki wyjatek w bloku
+    // poniewaz w kontekscie statycznym nie mozemy wypchac bledu do konstruktora
+    // mozna wpisac Runtime w bloku statycznym ale wyskoczy error Exception Initializer Error
+
+// finally - instrukcja, ktora zawsze sie wykona po rzuceniu wyjatku zanim strumien zostanie przerwany
+    // w fnally mozna rzucac kolejne wyjatki
+// dozwolone kombinacje:
+    // try, catch, finally
+    // try, finally
 
 // WYJATKI:
 //- Jesli wywolamy .get() na Optionalu, ktory jest pusty to dostaniemy wyjatek "NoValue Present"
