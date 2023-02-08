6da9ccd23af31371cb947c7210b68ba2f63b83e9
primalCodeMaker <binaryCodeMaker@gmail.com>
2023-02-08 03:33:19 +0100

    Comparable

--- Generics/src/comparingObjects/INFO.java
+++ Generics/src/comparingObjects/INFO.java
@@ -2,7 +2,15 @@ package comparingObjects;
 
 public class INFO {
 }
-
-// Comparator<> to interfejs funkcyjny zawierajacy 1 metode compare
+// Comparator
+// Comparator<> to interfejs funkcyjny zawierajacy 1 metode compare()
 // przyjmuje oan 2 obiekty i zwraca inta
+// na tej podstawie porownuje obiekty w calej lsicie i uklada je zgodnie z naszymi wytycznymi
+// caleca sie implementacje za pomoca lambd
 
+
+// Comparable
+// Comparable to tez interfejs
+// zawiera 1 metode compareTo()
+// mozna go zaimplementowac za pompca lambdy
+// ale jego przeznaczenie jest takie aby byl IMPLEMENTOWANY PRZEZ KLASE
