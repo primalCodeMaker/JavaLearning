c55dec408e76934a17385005fd230896b5f166c0
primalCodeMaker <binaryCodeMaker@gmail.com>
2023-02-08 04:46:35 +0100

    info updated

--- Generics/src/comparingObjects/INFO.java
+++ Generics/src/comparingObjects/INFO.java
@@ -14,3 +14,9 @@ public class INFO {
 // zawiera 1 metode compareTo()
 // mozna go zaimplementowac za pompca lambdy
 // ale jego przeznaczenie jest takie aby byl IMPLEMENTOWANY PRZEZ KLASE
+
+// Klasy typu Integer, Double, String... implementuja Comparable
+// tak wiec mozna na nich wywolywac natural.order()
+// na wlasnych klasach sami musimy implementowac Comparable, albo definiowac swoj Comparator.
+    // ianczej nie bedziemy mieli dostepu do zadnej formy sortowania
+
