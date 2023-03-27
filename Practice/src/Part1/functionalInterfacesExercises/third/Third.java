0b29420c0802038f83f0e269b25b88d0a33a744a
primalCodeMaker <binaryCodeMaker@gmail.com>
2023-03-27 22:45:51 +0200

    Exercise3 Started

--- Practice/src/Part1/functionalInterfacesExercises/third/Third.java
+++ Practice/src/Part1/functionalInterfacesExercises/third/Third.java
@@ -1,8 +1,51 @@
 package Part1.functionalInterfacesExercises.third;
 
+import java.util.Objects;
+import java.util.function.BinaryOperator;
+
 public class Third {
 
     public static void main(String[] args) {
 
+        House house1 = new House(2);
+        House house2 = new House(2);
+        House house3 = new House(3);
+
+       BinaryOperator<House> binaryOp = (a, b) -> {
+
+
+           return null;
+       };
+
+       binaryOp.apply(house1, house3);
+
+    }
+
+    private static class House {
+        private final Integer numberOfRooms;
+
+        public House(Integer numberOfRooms) {
+            this.numberOfRooms = numberOfRooms;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            House house = (House) o;
+            return Objects.equals(numberOfRooms, house.numberOfRooms);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(numberOfRooms);
+        }
+
+        @Override
+        public String toString() {
+            return "House{" +
+                    "numberOfRooms=" + numberOfRooms +
+                    '}';
+        }
     }
 }
