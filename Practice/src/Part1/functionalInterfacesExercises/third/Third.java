9594909df57bd066108ace7b583721cf44779e51
primalCodeMaker <binaryCodeMaker@gmail.com>
2023-03-28 18:25:23 +0200

    Exercise3 Done

--- Practice/src/Part1/functionalInterfacesExercises/third/Third.java
+++ Practice/src/Part1/functionalInterfacesExercises/third/Third.java
@@ -1,6 +1,6 @@
 package Part1.functionalInterfacesExercises.third;
 
-import java.util.Objects;
+import java.util.*;
 import java.util.function.BinaryOperator;
 
 public class Third {
@@ -11,15 +11,29 @@ public class Third {
         House house2 = new House(2);
         House house3 = new House(3);
 
-       BinaryOperator<House> binaryOp = (a, b) -> {
+        BinaryOperator<House> binaryOp = (a, b) -> {
+            return whichHousisBiggest(a, b);
+        };
 
-
-           return null;
-       };
-
-       binaryOp.apply(house1, house3);
+        House selectHouse = binaryOp.apply(house1, house3);
+        House checking = Optional.ofNullable(selectHouse).orElseThrow(() -> new RuntimeException("AHTUNG NULL"));
+        System.out.println(checking);
 
     }
+
+    private static House whichHousisBiggest(House a, House b) {
+        List<House> newList = new ArrayList<>();
+        newList.add(a);
+        newList.add(b);
+        if (!a.equals(b)) {
+            Comparator<House> bigest = ((h1, h2) -> h1.numberOfRooms.compareTo(h2.numberOfRooms));
+            newList.sort(bigest.reversed());
+            return newList.get(0);
+        } else {
+            return null;
+        }
+    }
+
 
     private static class House {
         private final Integer numberOfRooms;
@@ -34,6 +48,10 @@ public class Third {
             if (o == null || getClass() != o.getClass()) return false;
             House house = (House) o;
             return Objects.equals(numberOfRooms, house.numberOfRooms);
+        }
+
+        public Integer getNumberOfRooms() {
+            return numberOfRooms;
         }
 
         @Override
