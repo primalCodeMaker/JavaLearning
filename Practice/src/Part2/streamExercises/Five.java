1bdb3ef955bf9d8ed0cbed89fa31bdb3ee41db81
primalCodeMaker <binaryCodeMaker@gmail.com>
2023-04-19 22:03:15 +0200

    exercised done

--- Practice/src/Part2/streamExercises/Five.java
+++ Practice/src/Part2/streamExercises/Five.java
@@ -1,18 +1,25 @@
 package Part2.streamExercises;
 
-import java.util.Arrays;
-
 public class Five {
 
     // Jeżeli podpowiem, że String posiada metodę .chars(), która generuje IntStream, spróbuj
     // zaimplementować sprawdzanie, czy słowo/zdanie jest palindromem, przy wykorzystaniu Streamów.
 
     public static void main(String[] args) {
-        String sedes = "sedes";
-        String noSedes = "noSedes";
+        String s1 = "sedes";
+        String s2 = "noSedes";
 
-//        Arrays.stream(sedes.chars().boxed()
-//                .filter(a -> a.compareTo(sedes.length()));
+        System.out.println(s1 + " " + isPalindrom(s1));
 
+        System.out.println(s2 + " " + isPalindrom(s2));
+
+    }
+
+    private static boolean isPalindrom(String sth) {
+        String toStreingReversed = sth.chars()
+                .mapToObj(c -> String.valueOf((char) c))
+                .reduce("", (left, right) -> right.concat(left));
+
+        return toStreingReversed.equals(sth);
     }
 }
