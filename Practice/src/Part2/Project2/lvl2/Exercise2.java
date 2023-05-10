6303b41ee010add2b01789a9418ae2f53f8d6195
primalCodeMaker <binaryCodeMaker@gmail.com>
2023-05-10 13:20:42 +0200

    repeate material after break

---
+++ Practice/src/Part2/Project2/lvl2/Exercise2.java
@@ -0,0 +1,13 @@
+package Part2.Project2.lvl2;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+
+//        Przygotuj metodę, która przyjmie konkretną kategorię i dla tej kategorii zwróci mapę,
+//        gdzie kluczem będzie id klienta,
+//        a wartością ilość kupionych przez niego produktów z podanej kategorii
+//        (weź pod uwagę tylko te transakcje, w których ilość kupionych produktów jest większa niż 1).
+
+    }
+}
