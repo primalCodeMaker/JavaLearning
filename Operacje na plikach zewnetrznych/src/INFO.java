32aa7afdb95040f898d36590a25e5601d6143751
primalCodeMaker <binaryCodeMaker@gmail.com>
2023-06-12 23:46:47 +0200

    Resources Boundle summary

--- Operacje na plikach zewnetrznych/src/INFO.java
+++ Operacje na plikach zewnetrznych/src/INFO.java
@@ -7,5 +7,15 @@ public class INFO {
     // LOKALIZACJA - Faktyczne dostosowanie oprogramowania do uzywania danego jezyka w stosunku do regionu
         // l10n << skrot
 
+    // w Javie mozna robic paczki zwane "boundle" w ktorych przetrzymujemy pakiety jezykowe.
+        // dzialaja one na bazie klucz = wartosc
+        // pozniej w kodzie mozemy wolac wartosci za pomoca kluczy.
+            // Dla kazdej paczki jezykowej klucze powinny byc takie same, albo powinny byc zinicjaliozowane w paczce
+                    // szczegoly w: languages >> languageChanging
+
+    // aby zrobic paczke jako boundle wchodzimy do opcji ctrl+alt+shift+s >> Modules i zaznaczamy na folderze opcje resources
+        // taki folder dostaje rozszerzenie .properties
+
+
 
 }
