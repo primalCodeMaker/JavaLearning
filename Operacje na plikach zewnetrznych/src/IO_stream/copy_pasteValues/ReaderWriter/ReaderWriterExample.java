879b7cf35e19085b9c2acabf792acdf56ae2be61
primalCodeMaker <binaryCodeMaker@gmail.com>
2023-11-01 22:59:26 +0100

    BufferedReader / BufferedWriter

--- Operacje na plikach zewnetrznych/src/IO_stream/copy_pasteValues/ReaderWriter/ReaderWriterExample.java
+++ Operacje na plikach zewnetrznych/src/IO_stream/copy_pasteValues/ReaderWriter/ReaderWriterExample.java
@@ -1,7 +1,10 @@
 package IO_stream.copy_pasteValues.ReaderWriter;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ReaderWriterExample {
@@ -13,14 +16,35 @@ public class ReaderWriterExample {
         File outputFile = new File("src/IO_stream/copy_pasteValues/someOutputFile.txt");
 
         List<String> fileRead = readFile(inputFile);
+        writeFile(outputFile, fileRead);
 
     }
 
-    private static List<String> readFile(final File inputFile) {
-        try (
-//                todo 3:05
-                ) {
 
+    private static List<String> readFile(final File inputFile) throws IOException {
+        List<String> result = new ArrayList<>();
+        try (
+                BufferedReader buffReader = new BufferedReader(new FileReader(inputFile, StandardCharsets.UTF_8))) {
+
+            String lineReader = buffReader.readLine();
+            while (lineReader != null) {
+                result.add(lineReader);
+                lineReader = buffReader.readLine();
+
+            }
+
+        }
+        return result;
+    }
+
+    private static void writeFile(File outputFile, List<String> fileRead) throws IOException {
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile, StandardCharsets.UTF_8))) {
+            for (String line : fileRead) {
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+                
+            }
         }
     }
 
