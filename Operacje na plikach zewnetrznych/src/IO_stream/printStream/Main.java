77801dac41f9dcd976d8e6914f15ffc7e1939529
primalCodeMaker <binaryCodeMaker@gmail.com>
2023-11-03 21:42:02 +0100

    Print Stream

---
+++ Operacje na plikach zewnetrznych/src/IO_stream/printStream/Main.java
@@ -0,0 +1,29 @@
+package IO_stream.printStream;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("PrinterStreamExample.txt");
+        try (PrintWriter printWriter = new PrintWriter(
+                new BufferedWriter(
+                        new FileWriter(file)))) {
+
+            printWriter.print(1L);
+            printWriter.write(String.valueOf(1L));
+
+            Car car = new Car("Samochod Stefan");
+            printWriter.print(car);
+
+            printWriter.println();
+
+            printWriter.println("zajavka");
+
+            printWriter.printf("some value: %s%n ", car.toString());
+
+        }
+    }
+}
+
