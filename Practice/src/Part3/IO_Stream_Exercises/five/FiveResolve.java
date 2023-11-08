22705c31fbc36165296b7f46088612dc15620c8e
primalCodeMaker <binaryCodeMaker@gmail.com>
2023-11-08 05:32:15 +0100

    exercise5 done!

--- Practice/src/Part3/IO_Stream_Exercises/five/FiveResolve.java
+++ Practice/src/Part3/IO_Stream_Exercises/five/FiveResolve.java
@@ -1,14 +1,12 @@
 package Part3.IO_Stream_Exercises.five;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,19 +18,21 @@ public class FiveResolve {
 
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("./src/Part3/workingCatalog/CatalogInside2/anotherFile.txt");
-
         printSpecyficWord(path);
 
     }
 
     private static void printSpecyficWord(Path path) throws IOException {
-        Files.lines(path)
+        TreeMap<Integer, List<String>> searchingValues = Files.lines(path)
                 .map(charr -> charr.replace(".", "").replace(",", ""))
                 .flatMap(lines -> Stream.of(lines.split(" ")))
-                .collect(Collectors.groupingBy(key -> key.length()))
-                .forEach((a, b) -> System.out.println(a.equals(3)));
-//                .forEach((a, b) -> a.equals(3) && (a.MAX_VALUE), b-> System.out.println(b));
+                .collect(Collectors.groupingBy(key -> key.length(), TreeMap::new, Collectors.toList()));
 
+        Integer maxLength = searchingValues.keySet().stream().max(Comparator.comparingInt(a -> a)).orElseThrow();
 
+        System.out.println("second 3-letter word: " + searchingValues.get(3).get(1));
+        System.out.println("first longest word: " + searchingValues.get(maxLength).get(1));
     }
+
 }
+
