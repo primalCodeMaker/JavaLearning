f11aea7f5ad4d14737c0ce7afcd50a9f9a68a872
primalCodeMaker <binaryCodeMaker@gmail.com>
2023-11-10 09:01:51 +0100

    exercise seven done

--- Practice/src/Part3/IO_Stream_Exercises/seven/SevenResolve.java
+++ Practice/src/Part3/IO_Stream_Exercises/seven/SevenResolve.java
@@ -1,13 +1,12 @@
 package Part3.IO_Stream_Exercises.seven;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SevenResolve {
 
@@ -16,25 +15,22 @@ public class SevenResolve {
     Do wygenerowania zawartości pliku wykorzystaj https://pl.lipsum.com
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Path path = Paths.get("./src/Part3/workingCatalog/CatalogInside2/anotherFile.txt");
 
-        try {
-            amountOfWorl(path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+        amountOfWorl(path);
 
     }
 
     private static void amountOfWorl(Path path) throws IOException {
 
-        List<String[]> collect = Files.lines(path)
-                .map(s -> s.split(" "))
-                .collect(Collectors.toList())
-                .forEach(a -> Arrays.stream(a).count());
+        List<String> collect = Files.lines(path)
+                .filter(a -> !a.isEmpty())
+                .flatMap(lines -> Stream.of(lines.split(" ")))
+                .collect(Collectors.toList());
 
-//        return count;
+        System.out.println(collect);
+        System.out.println(collect.stream().count());
+
     }
 }
