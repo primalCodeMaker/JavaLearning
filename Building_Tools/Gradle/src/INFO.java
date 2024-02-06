ec21dd45a35f62cecccc7c63e7088a7e1b85df03
primalCodeMaker <binaryCodeMaker@gmail.com>
2024-02-06 01:30:44 +0100

    gradle tasks part2, and manifest

--- Building_Tools/Gradle/src/INFO.java
+++ Building_Tools/Gradle/src/INFO.java
@@ -1,5 +1,10 @@
 public class INFO {
     /*
+    ctrl + alt + shitft + C na zaznaczonej klasie
+    kopiuje pelna sciezke do tej klasy, np klasa Main:
+                                pl.gradleExample.Main
+
+
 Gradle rozni sie od Mavena tym, ze Maven jest gotowym szkieletem, w ktory wkladamy pluginy i dependencje
     a Gradle jest osobnym programem, ktory sami piszemy, skladamy i konfigurujemy.
     Mozemy mu dodawac wlasnie opcje, ktore beda dzialac na etapie runtimu jak: println
@@ -35,7 +40,7 @@ Gradle rozni sie od Mavena tym, ze Maven jest gotowym szkieletem, w ktory wklada
         gradlew - v dla ustawien javy w systemie operacyjnym
 
 Nasza klasa main nadal nie jest odczytywana jako klasa javowa i nie mozna jej odpalic
-    build.gradle nalezy dodac adnotacje     apply plugin: 'java'
+    build.gradle nalezy dodac plugin     apply plugin: 'java'
     w build.gradle mozna rowniez ustawic sourceCompatibility = '18' wersje kompatybilnosci z java dla projektu
     oraz parametr version '0.1.0' << ustalamy w jakiej wersji jest nasz projek
 
@@ -43,8 +48,14 @@ Nasza klasa main nadal nie jest odczytywana jako klasa javowa i nie mozna jej od
         ustalamy rozne wersje compilacji i runtimu dla aplikacji.
         Jesli nie wpiszemy targetCompatibility to Gradle samoczynnie ustawi ta sama wersje jak dla sourceCompatibility
 
-
-
+aby wskazac gdzie jest metoda urchomieniowa naszego pliku musimy ja podac poprzez manifest jak w Mavenie:
+ar {
+    archivesBaseName = 'myGradle-jar-new'     <<< nazywamy nasz plik uruchomieniowy
+    manifest {
+        attributes 'Main-Class': "pl.gradleExample.Main"  <<< podajemy sciezke katalogowa do niego
+    }
+}
+z terminala odpalamy: java -jar .\myGradle-jar-new-0.1.0.jar <<< sama nazwa pliku z build.lib z rozszerzeniem jar
 
 
 
@@ -67,6 +78,14 @@ Aby odpalic gradle z terminala wpisujemy: gradle nazwaTaskaKtoryChcemyOdpalic
 przy uruchomieniu komendy gradle tasks wyswietli nam wsystkie dostepne takski dla projektu w gradle
     z paramerem --all pokaze rowniez nasze napisane taski
 
+Taski w gradlu nie są kaskadowe jak w Mavenie tylko niektore zaleza od siebie a od innych nie
+np: Build zalezy od - check i assemble
+    check zalezny od test
+    test zalezy od testClasses\
+Wszystko mozna sprawdzic w dokumentacji
+Jeśli wybierzemy jakiś, task ktory jest zalezny od innych to po kolei beda sie one wywolywac
+
+
 Isnieje rowniez koncepcja taskow domyslnych:
 defaultTasks 'ourFirstTask', 'ourThirTask'
     mozemy w nim podac kilka taskow ktore chcemy odpalic jeden po drugim
@@ -82,7 +101,6 @@ Taski moga wzajemnie od siebie zalezec:
              println 'afterCompile task runing'
         }
     }
-
     lub
 
     afterClean.dependsOn clean
