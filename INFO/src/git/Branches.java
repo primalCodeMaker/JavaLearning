8d7f1e1303bed9d396e6fece2c2c0e75bb2250cf
primalCodeMaker <binaryCodeMaker@gmail.com>
2024-03-13 04:55:58 +0100

    git branches basic info

---
+++ INFO/src/git/Branches.java
@@ -0,0 +1,36 @@
+package git;
+
+public class Branches {
+
+    /*
+
+    git branch          >> sprawdza na jakim branchu jestesmy
+    git branch -l "m*"  >> znajduje branche ktore zaczynaja sie od "m"
+
+    git branch nazwa_nowego_brancha  >> tworzy nowego brancha ale nie ustawia go jako galaz robocza
+        nowa galaz jest dokladna kopia stanu jaki znajdowal sie na galezi main w momencie tworzenia go
+        nowy branch nie ma dostepu do nowych zmian na galezi main dopoki nie zrobimy merga
+        sledzone zmiany musza zostac zakomitowane zanim GIT pozwoli nam zalozyc nowego brancha
+
+    git branch najnowszy_branch jakis_branch_ktory_byl >> tworzy najnowszy branch w projekcie
+        na podstawie jakiegos_brancha_ktory_byl wczesniej
+
+    git branch nazwa_brancha_naPodstawie_commita fjgh4h52...  >> twozy branch na podstawie commita ktory wybierzemy
+
+    git checkout nazwa_nowego_brancha >> zmienia galaz na ktorej chcemy pracowac
+        commity dodaja sie na branchu na ktorym aktualnie jestesmy
+
+
+
+
+
+
+
+
+
+
+
+     */
+
+
+}
