5e79b7002e28cbda69025b3d1df8ad0ca7c4c463
primalCodeMaker <binaryCodeMaker@gmail.com>
2024-04-05 20:39:47 +0200

    Cherry Pick

---
+++ INFO/src/git/CherryPick.java
@@ -0,0 +1,15 @@
+package git;
+
+public class CherryPick {
+
+    /*
+    Chery Pick
+        sluzy do zabrania jakiegos commita z jednej galezi i przeniesienie go na inna galaz
+        podczas robienia cherry picka commit stan kodu w commicie zostaje skopiowany
+        ale zmienia sie ID tego commita oraz Parent na branchu do ktorego go wklejamy
+
+        jesli podczas cherry picka powstaja konflikty, ktorych nie mozemy rozwiazac
+        cherry-pick --skip >> anuluje probe zrobienia cherry picka
+
+     */
+}
