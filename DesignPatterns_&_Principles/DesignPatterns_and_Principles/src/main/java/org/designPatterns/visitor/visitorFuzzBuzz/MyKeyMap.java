fffa2223e40a76cc4dd353040f88452f23a9a863
primalCodeMaker <binaryCodeMaker@gmail.com>
2024-07-31 20:35:51 +0200

    Visitor FuzzBuzz solution

---
+++ DesignPatterns_&_Principles/DesignPatterns_and_Principles/src/main/java/org/designPatterns/visitor/visitorFuzzBuzz/MyKeyMap.java
@@ -0,0 +1,29 @@
+package org.designPatterns.visitor.visitorFuzzBuzz;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class MyKeyMap implements Visitor {
+
+    Map<Function<Integer, Boolean>, String> functionMap = Map.of(
+            x -> (x % 3 == 0), "Buzz",
+            x -> x % 5 == 0, "Fuzz",
+            x -> x % 5 == 0 && x % 3 == 0, "BuzzFuzz"
+    );
+
+    @Override
+    public void visit(List<Integer> list) {
+        list.forEach(item -> {
+            String result = functionMap.entrySet().stream()
+                    .filter(entry -> entry.getKey().apply(item))
+                    .map(Map.Entry::getValue)
+                    .findFirst()
+                    .orElse("No Match");
+            System.out.println("For value " + item + ": " + result);
+        });
+    }
+}
+
+
+
---
+++ DesignPatterns_&_Principles/DesignPatterns_and_Principles/src/main/java/org/designPatterns/visitor/visitorFuzzBuzz/Visitor.java
@@ -0,0 +1,8 @@
+package org.designPatterns.visitor.visitorFuzzBuzz;
+
+import java.util.List;
+
+public interface Visitor {
+
+    void visit(List<Integer> list);
+}
