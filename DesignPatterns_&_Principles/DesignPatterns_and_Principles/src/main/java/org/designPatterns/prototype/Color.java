160475b3bf5e08134fa944390110f8046043a079
primalCodeMaker <binaryCodeMaker@gmail.com>
2024-07-31 22:30:30 +0200

    Prototype Examples

---
+++ DesignPatterns_&_Principles/DesignPatterns_and_Principles/src/main/java/org/designPatterns/prototype/Color.java
@@ -0,0 +1,15 @@
+package org.designPatterns.prototype;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+import lombok.Value;
+
+@ToString
+@Value
+@AllArgsConstructor
+
+public class Color {
+
+    final String color;
+
+}
