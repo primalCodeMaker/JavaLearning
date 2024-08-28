132116e125eefc2bbf525578b64de68818f0b3a9
primalCodeMaker <binaryCodeMaker@gmail.com>
2024-08-28 20:26:14 +0200

    Tests Started

---
+++ Testing/src/main/java/org/example/INFO.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class INFO {
+
+    /*
+    JUnit - framework do pisania testów
+    TestNG - inny framework
+    Mockito - framework do mockowania
+
+        JUnit sklada sie z 3 modolow:
+    JUnit Platform - platfirma do uruchamiania roznych frameworkow testowych na JVM.
+        Interface pomiedzy JUnit a narzedziami do budowania.
+        Pozwala skonfigurowac projekt tak aby Testy byly uruchamiane na etapie buildu
+
+    JUnit Jupiter - modol ktory wprowadza nowe adnotacje w wersji 5 w stosunku do poprzednich wersji
+    JUnit Vintage - jesli mamy napisane testy w wersji JUnit ponizej 5 to potrzebujemy tego modulu do odpalenia ich
+
+     */
+}
